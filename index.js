0b27783a617ee12cb3127118693490ba16896987
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2019-09-01 12:07:24 -0400

    added a pause

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 let time = moment();
-let startTime, endTime, remindTime;
+let startTime, remindTime, endTime;
+let timeLeft = 90 * 60;
+let stopped = true;
 const sound = new Audio('alarm.mp3');
 
 const updateTime = () => {
   time = moment();
   $('#ctime').html(time.format('h:m:s'));
-;}
+  if (stopped) return;
+  const diff = endTime.diff(time, 'seconds')
+  if (diff < 1) {
+    stopped = true;
+    return sound.play();
+  }
+  $('#time').html(diff);
+  if (remindTime) {
+    const rdiff = remindTime.diff(time, 'seconds');
+    if (rdiff < 1) {
+      startTime = moment();
+      remindTime = undefined;
+      sound.play();
+    }
+    $('#rtime').html(remindTime.diff(time, 'minutes'));
+  }
+};
 
 $(document).ready(() => {
+  startTime = moment();
   updateTime();
   setInterval(updateTime, 1000);
-  $('#start').one('click', () => {
-    endTime = time.add(90, 'minutes');
-    startTime = moment();
-    setInterval(() => {
-      $('#time').html(endTime.diff(time, 'minutes'));
-      if (remindTime) {
-        const diff = remindTime.diff(time, 'seconds');
-        if (diff < 1) {
-          startTime = moment();
-          remindTime = undefined;
-          sound.play();
-        }
-        $('#rtime').html(remindTime.diff(time, 'minutes'));
-      }
-    }, 1000);
+  $('#start').click(() => {
+    stopped = !stopped;
+    if (stopped) {
+      $('#start').html('start');
+      timeLeft = endTime.diff(time, 'seconds');
+    }
+    else {
+      $('#start').html('stop');
+      time = moment();
+      endTime = time.clone().add(timeLeft, 'seconds');
+    }
   });
   $('#rconfirm').click(() => {
     const rvalue = parseInt($('#rvalue').val());
